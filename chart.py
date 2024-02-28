588683616c69377f7e36ba3d9d99ba21cacbd432
John Jeremie Jacinto <140291756+jayzerk@users.noreply.github.com>
2024-02-29 07:45:43 +0800

    Binary/Linear

--- chart.py
+++ chart.py
@@ -9,3 +9,27 @@ class showCharts:
         plt.xlabel = "Number of Elements"
         plt.ylabel = "Number of Iterations"
         plt.show()
+        
+    def plotChart(length1, length2, length3,
+                  length4, length5, length6,
+                  iteration):
+        plt.xlim(0, 100)
+        plt.subplot(1,3,1)
+        plt.scatter(length1, iteration, color='red', alpha=0.4)
+        
+        plt.subplot(1,3,2)
+        plt.scatter(length2, iteration, color='red', alpha=0.4)
+
+        plt.subplot(1,3,3)
+        plt.scatter(length3, iteration, color='red', alpha=0.4)
+        
+        plt.subplot(2,3,1)
+        plt.scatter(length4, iteration, color='blue', alpha=0.4)
+        
+        plt.subplot(2,3,2)
+        plt.scatter(length5, iteration, color='blue', alpha=0.4)
+        
+        plt.subplot(2,3,3)
+        plt.scatter(length6, iteration, color='blue', alpha=0.4)
+        plt.tight_layout()
+        plt.show()
--- main.py
+++ main.py
@@ -2,26 +2,64 @@ from chart import showCharts
 import matplotlib.pyplot as plt
 import random
 
-searchValue = [random.randint(1, 10000) for _ in range(10000)]
-arrayNumber = list(range(1,10001))
-
-xaxis = []
-yaxis = []
-runChart = showCharts()
+maxDigits = []
+searchValues = []
+iterationValues = []
+iterationValuesBinary = []
+arrayNumber = list(range(1,101))
 
 def searchAlgo(num, searchFor):
-    iteration = 1
-    for numArrayTicks in num:
-        if searchFor == numArrayTicks:
-            break
-        else:
-            iteration += 1
-    return iteration
+    loopValues = []
+    for search in searchFor:
+        iteration = 0
+        for ticks in num:
+            if search == ticks:
+                break
+            else:
+                iteration += 1
+        loopValues.append(iteration)
+    return loopValues
+def binarySearch(num, searchFor):
+    loopValuesBinary = []
+    for search in searchFor:
+        left = 0
+        right = 99
+        midpoint = (right+left)//2
+        repeat = 1
+        iteration = 0
+        while(repeat):
+            if (search == num[midpoint]) or (right < left) or (right == left):
+                repeat = 0
+                break
+            elif search > num[midpoint]:
+                print("search > "+str(search)+" : "+str(left)+" : "+str(right)+" : "+str(midpoint)+" : "+str(num[midpoint]))
+                left = midpoint + 1
+                iteration += 1
+                midpoint = (right+left)//2
+            elif search < num[midpoint]:
+                print("search < "+str(search)+" : "+str(left)+" : "+str(right)+" : "+str(midpoint)+" : "+str(num[midpoint]))
+                right = midpoint - 1
+                iteration += 1
+                midpoint = (right+left)//2
+            
+        loopValuesBinary.append(iteration)
+    return loopValuesBinary
+for i in range(3):
+    inputDigit = int(input("Enter max digit #{}: ".format(i+1)))
+    maxDigits.append(inputDigit)
+    searchValues.append([random.randint(1, inputDigit) for _ in range(100)])
+    iterationValues.append(searchAlgo(arrayNumber, searchValues[i]))
+    iterationValuesBinary.append(binarySearch(arrayNumber, searchValues[i]))
 
-for indexX, value in enumerate(searchValue):
-    xaxis.append(searchAlgo(arrayNumber, searchValue[indexX]))
-    
-runChart.lineChart(xaxis,arrayNumber)
+showCharts.plotChart(iterationValues[0],
+                     iterationValues[1],
+                     iterationValues[2],
+                     iterationValuesBinary[0],
+                     iterationValuesBinary[1],
+                     iterationValuesBinary[2],
+                     arrayNumber)
+
+
 
 
 
