d6c83ec3ff8cb2892ade51ba8c52483ce066685a
John Jeremie Jacinto <140291756+jayzerk@users.noreply.github.com>
2024-02-29 08:42:15 +0800

    update text

--- chart.py
+++ chart.py
@@ -3,28 +3,35 @@ import matplotlib.pyplot as plt
 class showCharts:
         
     def plotChart(iter_linear, iter_binary, arrayLength):
+        xlabel = 'Number of Iterations'
+        ylabel = 'Array Length'
+        #---------------------START OF LINEAR PLOT------------------
         highest_value, hv_count =showCharts.checkWorstCase(iter_linear)
         plt.subplot(1,2,1)
         plt.scatter(iter_linear, arrayLength, color='red', alpha=0.4)
         plt.ylim(0, max(arrayLength))
         plt.text(0.5, 1.05,
-                'Worst Case: {}'.format(highest_value)+' from {}'.format(max(arrayLength)),
+                'Linear Search Worst Case: {}'.format(highest_value)+' from {}'.format(max(arrayLength)),
                 ha='center', transform=plt.gca().transAxes)
         plt.text(.5, 1.01,
                 'Worst Case Count: {}'.format(hv_count)+' out of {}'.format(max(arrayLength)),
                 ha='center', transform=plt.gca().transAxes)
         highest_value, hv_count =showCharts.checkWorstCase(iter_binary)
-        
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        #---------------------START OF BINARY PLOT------------------
         plt.subplot(1,2,2)
         plt.scatter(iter_binary, arrayLength, color='red', alpha=0.4)
         plt.ylim(0, max(arrayLength))
         plt.text(0.5, 1.05,
-                'Worst Case: {}'.format(highest_value)+' from {}'.format(max(arrayLength)),
+                'Binary Search Worst Case: {}'.format(highest_value)+' from {}'.format(max(arrayLength)),
                 ha='center', transform=plt.gca().transAxes)
         plt.text(0.5, 1.01,
                 'Worst Case Count: {}'.format(hv_count)+' out of {}'.format(max(arrayLength)),
                 ha='center', transform=plt.gca().transAxes)
-
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        #---------------------END OF SUBPLOTS------------------
         plt.tight_layout()
         plt.show()
         
